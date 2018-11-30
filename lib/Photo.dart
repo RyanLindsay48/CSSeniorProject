77ff2c91f16ac3716abf8e3caeb6875480526daf
freitasa1 <43360025+freitasa1@users.noreply.github.com>
2018-11-30 01:25:09 -0500

    Create Photo.dart

---
+++ lib/Photo.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+class Photo {
+  final String filepath;
+  final int picture_id;
+  final String timestamp;
+
+  Photo(
+      {this.filepath, this.picture_id, this.timestamp});
+
+  //factory User.fromJson(Map<String, String> json) => _$UserFromJson(json);
+  //Map<String, String> toJson() => _$UserToJson(this);
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+        filepath: json['filepath'],
+        picture_id: json['picture_id'],
+        timestamp: json['timestamp']
+    );
+  }
+}
+
+//I will also look into why I created a picture and a photo class
+//Again, I am very tired
