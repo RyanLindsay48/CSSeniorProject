c9914dfeeab711a51663368c114b31320f17ce53
freitasa1 <43360025+freitasa1@users.noreply.github.com>
2018-11-30 01:20:11 -0500

    Create ExposureList.dart

---
+++ lib/ExposureList.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:actualhousehawk/Exposure.dart';
+
+import 'Photo.dart';
+class ExposureList {
+  final List<Exposure> exposures;
+
+  ExposureList(
+      {this.exposures});
+
+  factory ExposureList.fromJson(List<dynamic> parsedJson) {
+    List<Exposure> exposures = new List<Exposure>();
+    exposures = parsedJson.map((i)=>Exposure.fromJson(i)).toList();
+    return new ExposureList(
+        exposures: exposures,
+    );
+  }
+}
