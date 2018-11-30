6b1dfb31bd69039bff708c7f35465e488adfd5c7
freitasa1 <43360025+freitasa1@users.noreply.github.com>
2018-11-30 01:19:44 -0500

    Create Exposure.dart

---
+++ lib/Exposure.dart
@@ -0,0 +1,26 @@
+library actualhousehawk.Exposure;
+
+
+class Exposure {
+  final String end_time;
+  final int exposures_id;
+  final int pi_id;
+  final String start_time;
+  final int user_id;
+
+  Exposure(
+      {this.end_time, this.exposures_id, this.pi_id, this.start_time, this.user_id});
+
+  //factory User.fromJson(Map<String, String> json) => _$UserFromJson(json);
+  //Map<String, String> toJson() => _$UserToJson(this);
+
+  factory Exposure.fromJson(Map<String, dynamic> json) {
+    return Exposure(
+        end_time: json['end_time'],
+        exposures_id: json['exposures_id'],
+        pi_id: json['pi_id'],
+        start_time: json['start_time'],
+        user_id: json['user_id']
+    );
+  }
+}
