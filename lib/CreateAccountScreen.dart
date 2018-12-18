98e2360d842392258a4fd788e914c0146f8fa2d2
Domenick Palmiotto <32915164+dpalm8@users.noreply.github.com>
2018-12-17 22:54:44 -0500

    working create account with validators

--- lib/CreateAccountScreen.dart
+++ lib/CreateAccountScreen.dart
@@ -17,8 +17,7 @@ class CreateAccountScreen extends StatefulWidget {
   CreateAccountScreenState createState() => CreateAccountScreenState();
 }
 
-class CreateAccountScreenState extends State<CreateAccountScreen>{
-
+class CreateAccountScreenState extends State<CreateAccountScreen> {
   static String fname = '';
   static String lname = '';
   static String email = '';
@@ -33,185 +32,198 @@ class CreateAccountScreenState extends State<CreateAccountScreen>{
   final formKey = GlobalKey<FormState>();
 
   @override
-  initState(){
+  initState() {
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-      return new MaterialApp(
+    return new MaterialApp(
         //title: 'Login page',
-          home: new Scaffold(
-              appBar: new AppBar(
+        home: new Scaffold(
+            appBar: new AppBar(
                 title: new Text(
                   'Create Account',
                 ),
-                  leading: IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginScreen()),
-                        );
-                      })
-              ),
-
-              body: new Padding(
+                leading: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    })),
+            body: new Padding(
                 padding: EdgeInsets.all(10.0),
-                  child: Form(
-                    key: formKey,
-                    child: ListView(children: <Widget>[
-                            TextFormField(
-                              decoration: new InputDecoration(
-                                  labelText: 'firstname'
-                              ),
-                              validator: (text)=> text ==''
-                                  ? 'FirstName is Required!'
-                                  : fname = text,
-                            ),
-                            TextFormField(
-                              decoration: new InputDecoration(
-                                  labelText: 'lastname'
-                              ),
-                              validator: (text)=> text ==''
-                                  ? 'Last Name is Required!'
-                                  : lname = text,
-                            ),
-                            TextFormField(
-                              decoration: new InputDecoration(
-                              labelText: 'email address'
-                            ),
-                            validator: (text)=> text ==''
-                                ? 'Invalid Email address!'
-                                : email = text,
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              decoration: new InputDecoration(
-                                labelText: 'Desired Password'
-                            ),
-                            validator: (text)=> text ==''
-                                ? 'Password is Required!'
-                                : password = text,
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              decoration: new InputDecoration(
-                                labelText: 'Confrim Desired Password'
-                              ),
-                              validator: (text)=> text == password
-                                ? 'Passwords do not match!'
-                                : password = text,
-                              ),
-                            TextFormField(
-                              decoration: new InputDecoration(
-                              labelText: 'Street Address'
-                              ),
-                              keyboardType: TextInputType.text,
-                              validator: (text)=> text ==''
-                                  ? 'Street Address is Required!'
-                                  : street_address = text,
-                            ),
-                            TextFormField(
-                              decoration: new InputDecoration(
-                              labelText: 'Apt. No'
-                              ),
-                              keyboardType: TextInputType.text,
-                              validator: (text){
-                                apartment_number = text;
-                              }
-                            ),
-                            TextFormField(
-                              decoration: new InputDecoration(
-                              labelText: 'City'
-                              ),
-                              keyboardType: TextInputType.text,
-                              validator: (text)=> text ==''
-                                  ? 'City is Required!'
-                                  : city = text,
-                            ),
-                            TextFormField(
-                                decoration: new InputDecoration(
-                                labelText: 'State'
-                                ),
-                                validator: (text)=> text ==''
-                                  ? 'State is Required!'
-                                  : state = text,
-                            ),
-                            TextFormField(
-                              decoration: new InputDecoration(
-                              labelText: 'Zip Code'
-                              ),
-                              validator: (text)=> text ==''
-                                ? 'Zip Code is Required!'
-                                : zip_code = text,
-                            ),
-                            RaisedButton(child: Text('Create Account'),
-                              onPressed: () => onPressed(context),
-                            ),
-                    ]),
-                  )
-              )
-          )
-      );
-    }
+                child: Form(
+                  key: formKey,
+                  child: ListView(children: <Widget>[
+                    TextFormField(
+                        decoration: new InputDecoration(labelText: 'firstname'),
+                        validator: (text) =>
+                            text == '' ? 'FirstName is Required!' : null,
+                        onSaved: (text) {
+                          fname = text;
+                        }),
+                    TextFormField(
+                        decoration: new InputDecoration(labelText: 'lastname'),
+                        validator: (text) =>
+                            text == '' ? 'Last Name is Required!' : null,
+                        onSaved: (text) {
+                          lname = text;
+                        }),
+                    TextFormField(
+                        decoration:
+                            new InputDecoration(labelText: 'email address'),
+                        validator: (text) =>
+                            text == '' ? 'Invalid Email address!' : null,
+                        onSaved: (text) {
+                          email = text;
+                        }),
+                    TextFormField(
+                        obscureText: true,
+                        decoration:
+                            new InputDecoration(labelText: 'Desired Password'),
+                        validator: (text) =>
+                            text == '' ? 'Password is Required!' : null,
+                        onSaved: (text) {
+                          password = text;
+                        }),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: new InputDecoration(
+                          labelText: 'Confrim Desired Password'),
+                      validator: (text) =>
+                          text != password ? 'Passwords do not match!' : null,
+                    ),
+                    TextFormField(
+                        decoration:
+                            new InputDecoration(labelText: 'Street Address'),
+                        keyboardType: TextInputType.text,
+                        validator: (text) =>
+                            text == '' ? 'Street Address is Required!' : null,
+                        onSaved: (text) {
+                          street_address = text;
+                        }),
+                    TextFormField(
+                        decoration: new InputDecoration(labelText: 'Apt. No'),
+                        keyboardType: TextInputType.text,
+                        onSaved: (text) {
+                          apartment_number = text;
+                        }),
+                    TextFormField(
+                        decoration: new InputDecoration(labelText: 'City'),
+                        keyboardType: TextInputType.text,
+                        validator: (text) =>
+                            text == '' ? 'City is Required!' : null,
+                        onSaved: (text) {
+                          city = text;
+                        }),
+                    TextFormField(
+                        decoration: new InputDecoration(labelText: 'State'),
+                        validator: (text) =>
+                            text == '' ? 'State is Required!' : null,
+                        onSaved: (text) {
+                          state = text;
+                        }),
+                    TextFormField(
+                        decoration: new InputDecoration(labelText: 'Zip Code'),
+                        validator: (text) =>
+                            text == '' ? 'Zip Code is Required!' : null,
+                        onSaved: (text) {
+                          zip_code = text;
+                        }),
+                    RaisedButton(
+                      child: Text('Create Account'),
+                      onPressed: () => onPressed(context),
+                    ),
+                  ]),
+                ))));
+  }
+
   void onPressed(BuildContext context) {
+    print('Button has been pressed about to validate');
     var form = formKey.currentState;
     if (form.validate()) {
+      print('From Validated');
       form.save();
       fetchData(context);
+    } else {
+      print('unable to validate');
     }
   }
 
   Future<User> fetchData(BuildContext context) async {
-    final response = await http.get(
-        'http://52.91.107.223:5000/users');
+    final response = await http.get('http://52.91.107.223:5000/users');
+    print(response.statusCode);
     if (response.statusCode == 200) {
+      print('create account fetch data successful connection');
       UserList userList = new UserList.fromJson(json.decode(response.body));
       int size = userList.users.length;
       int i = 0;
       int id = 1;
       var isValidEmail = true;
-      while((i < size) && isValidEmail){
-        if(userList.users[i].email_address == email){
+      while ((i < size) && isValidEmail) {
+        if (userList.users[i].email_address == email) {
           isValidEmail = false;
         }
-        if(userList.users[i].user_id > id){
+        if (userList.users[i].user_id > id) {
           id = userList.users[i].user_id;
         }
         i++;
       }
       user_id = id.toString();
-      if(isValidEmail == false){
+      if (isValidEmail == false) {
         print(email + 'is already in use, please enter a new email');
         print('NOT VALID EMAIL');
-      }
-      else{
+      } else {
         isValid = true;
         postData(context);
         print('VALID EMAIL CAN IT POST?');
         print(response.body);
       }
-    }
-    else{
+    } else {
       throw Exception('Failed to load post');
     }
   }
 
   Future<HttpClientResponse> postData(BuildContext context) async {
-    final response = await http.post(
-        'http://52.91.107.223:5000/user?email=' + email+'&password=' + password+'&fname=' +fname+'&lname=' + lname);
-    if(apartment_number == ''){
+    final response = await http.post('http://52.91.107.223:5000/user?email=' +
+        email +
+        '&password=' +
+        password +
+        '&fname=' +
+        fname +
+        '&lname=' +
+        lname);
+    if (apartment_number == '') {
       final response2 = await http.post(
-          'http://52.91.107.223:5000/household?user_id='+user_id+'&street_address='+street_address+'&city='+city+'&state='+state+'&zip_code='+zip_code);
+          'http://52.91.107.223:5000/household?user_id=' +
+              user_id +
+              '&street_address=' +
+              street_address +
+              '&city=' +
+              city +
+              '&state=' +
+              state +
+              '&zip_code=' +
+              zip_code);
     } else {
       final response3 = await http.post(
-          'http://52.91.107.223:5000/household?user_id='+user_id+'&street_address='+
-              street_address+'&apartment_num=' + apartment_number+ '&city='+city+'&state='+state+'&zip_code='+zip_code);
+          'http://52.91.107.223:5000/household?user_id=' +
+              user_id +
+              '&street_address=' +
+              street_address +
+              '&apartment_num=' +
+              apartment_number +
+              '&city=' +
+              city +
+              '&state=' +
+              state +
+              '&zip_code=' +
+              zip_code);
     }
-    Navigator.push(context,new MaterialPageRoute(builder: (context) => new LoginScreen()));
+    Navigator.push(context,
+        new MaterialPageRoute(builder: (context) => new LoginScreen()));
   }
 }
-
-
-
