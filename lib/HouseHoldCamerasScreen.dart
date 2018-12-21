e1703c390f632397fd12ef63aceef8a146c00f31
freitasa1 <43360025+freitasa1@users.noreply.github.com>
2018-12-20 22:53:44 -0500

    Update HouseHoldCamerasScreen.dart

--- lib/HouseHoldCamerasScreen.dart
+++ lib/HouseHoldCamerasScreen.dart
@@ -9,14 +9,23 @@ import 'ExposureList.dart';
 import 'Exposure.dart';
 import 'AddCamera.dart';
 
-
+/**
+* The HouseHoldCamerasScreen Class is used to show all of the cameras associated to a user.
+* Shown on this screen are buttons for each of the user's cameras, a back button that navigates back to the HomeScreen and a 
+* AddCamera Button that allows a user to add a camera to their account.
+*/
 class HouseHoldCamerasScreen extends StatelessWidget {
   List<Pi> pis = [];
 
   HouseHoldCamerasScreen(List<Pi> pis){
     this.pis = pis;
   }
-
+  
+  /*
+  * The fetchExposures method is used to get all of the exposures associated to a specific camera.
+  * It requests a json file of all of the exposures and decodes it into a list of Exposures. The user will then
+  * be redirected to the PiExposuresScreen where it will pass over the list of Exposures, the pi's location and the list of Pis too.
+  */
   Future<Exposure> fetchExposures(BuildContext context, Pi pi) async {
 
     final response = await http.get('http://ec2-52-91-107-223.compute-1.amazonaws.com:5000/pi/exposures?pi_id=' + pi.pi_id.toString());
@@ -47,7 +56,12 @@ class HouseHoldCamerasScreen extends StatelessWidget {
     }
     return buttons;
   }
-
+  /**
+  * The layout for this screen is a listView of buttons. Each of the users cameras are displayed as a button.
+  * When the user clicks on one of those cameras they will be redirected to the PisExposuresScreen where they will see
+  * All the exposures associated with that camera. It also contains a back button and a add camera button on the app bar.
+  * The back button redirects the user back to the HomeScreen and the add camera button redirects the user to the AddCamera Screen
+  */
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
