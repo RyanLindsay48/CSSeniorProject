0a42d24ee67ce8b626cf95c36c76dcd8eae4445e
freitasa1 <43360025+freitasa1@users.noreply.github.com>
2018-12-20 22:28:40 -0500

    Update ExposureList.dart

--- lib/ExposureList.dart
+++ lib/ExposureList.dart
@@ -1,11 +1,19 @@
 import 'Exposure.dart';
-
+/**
+* The ExposureList Class is used to decode a json file that contains a list of Exposures.
+* This class is used when we are trying to get all of the exposures associated to a one of the users
+* cameras. 
+*/
 class ExposureList {
   final List<Exposure> exposures;
 
   ExposureList(
       {this.exposures});
-
+  /**
+  * The ExposureList.fromJson method is used to decode a json file. The json file contains a list of Exposures
+  * The method takes each Exposure in the json file makes converts it into a Exposure Object and then it returns 
+  * The entire list of Exposure Objects.
+  */
   factory ExposureList.fromJson(List<dynamic> parsedJson) {
     List<Exposure> exposures = new List<Exposure>();
     exposures = parsedJson.map((i)=>Exposure.fromJson(i)).toList();
