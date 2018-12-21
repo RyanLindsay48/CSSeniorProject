575881f0da66ecf842b3edd5b9a306ca321b6a54
Joshua DeNoble <denoblej9@students.rowan.edu>
2018-12-21 00:36:11 -0500

    Removed weird nested files

--- app/CSSeniorProject/app/CSSeniorProject/app/testRecent.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import requests
-
-emailURL = 'http://52.91.107.223:5000/exposure/recent?user_id='
-
-user_id = '1'
-finalURL = emailURL+payload
-r = requests.get(finalURL)
-print(r.json)
