2ff93f2f0533380396d58b8f1d1a84d0837dc26c
freitasa1 <43360025+freitasa1@users.noreply.github.com>
2018-12-20 22:22:33 -0500

    Update Exposure.dart

--- lib/Exposure.dart
+++ lib/Exposure.dart
@@ -1,3 +1,8 @@
+
+/**
+* The Exposure class is used to decode a json file that we request from the server.
+* Once the file is decoded we extract the end_time, exposures_id, pi_id, start_time and user_id from the file
+*/
 class Exposure {
   final String end_time;
   final int exposures_id;
@@ -7,7 +12,10 @@ class Exposure {
 
   Exposure(
       {this.end_time, this.exposures_id, this.pi_id, this.start_time, this.user_id});
-
+  /**
+  * The Exposure.fromJson method is used to decode a json file and store the content from that
+  * file as an Exposure object.
+  */
   factory Exposure.fromJson(Map<String, dynamic> json) {
     return Exposure(
         end_time: json['end_time'],
